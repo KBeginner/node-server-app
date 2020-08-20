34c73c38c10d88e76051d796b5717b3fc26a613e
KBeginner <wyyking@126.com>
2020-08-20 14:51:26 +0800

    注册接口

---
+++ routes/user/register.js
@@ -0,0 +1,22 @@
+const {router, database, response} = require('../../utils/Middleware')
+
+router.post('/', (req, res)=>{
+    if (!req.body.account) {
+        res.json(response(400, 'account不能为空'))
+        return
+    } 
+    else if (!req.body.password) {
+        res.json(response(400, 'password不能为空'))
+        return
+    }
+    let sql = `insert into users (account, password) values ('${req.body.account}', '${req.body.password}')`
+    database.query(sql, (err, data)=>{
+        if (err && err.sqlState==23000) {
+            res.json(response(400, '该账号已注册'))
+            return
+        }
+        err ? res.json(response(500, err)) : res.json(response(200, '注册成功'))
+    })
+})
+
+module.exports = router

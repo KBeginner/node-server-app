1eb955f3d8a00f5ba6965dc20ad9adbe2e5aa5cc
KBeginner <wyyking@126.com>
2020-08-20 14:51:13 +0800

    获取用户信息接口

---
+++ routes/user/getUser.js
@@ -0,0 +1,13 @@
+const {router, database, response} = require('../../utils/Middleware')
+
+router.get('/', (req, res)=>{
+    if (!req.query.id) {
+        res.json(response(400, 'id不能为空'))
+        return
+    }
+    database.query(`select * from users where id=${req.query.id}`, (err, data)=>{
+        err ? res.json(response(500, err)) : res.json(response(200, data))
+    })
+})
+
+module.exports = router

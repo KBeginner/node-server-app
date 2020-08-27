2a4a8b0f92ca31c2a67545ba6eb7937b1c14dd18
KBeginner <wyyking@126.com>
2020-08-27 09:47:03 +0800

    获取笔记接口

--- router/note/getNote.js
+++ router/note/getNote.js
@@ -1,24 +1,29 @@
-const {router, database, response} = require('../../utils/Middleware')
+const {router, database, response, path} = require('../../utils/Middleware')
 
 router.get('/note/getNote', (req, res)=>{
-    console.log(__dirname)
-    res.sendFile('E:/Workspace/node-server-app/uploads/df53614bebb0b08b2b76c19015765838' );
-    // if (!req.query.id) {
-    //     res.json(response(400, 'id不能为空'))
-    //     return
-    // }
-    // let result = new Object()
-    // database.query(`select * from notes where id='${req.query.id}'`, (err, data)=>{
-    //     if (err) {
-    //         res.json(response(500, err))
-    //     } else {
-    //         result.id = data[0].id
-    //         result.userId = data[0].userId
-    //         result.title = data[0].title
-    //         result.content = data[0].content
-    //         res.json(response(200, result))
-    //     }
-    // })
+    if (!req.query.id) {
+        res.json(response(400, 'id不能为空'))
+        return
+    }
+    let result = new Object()
+    database.query(`select * from notes where id='${req.query.id}'`, (noteErr, noteData)=>{
+        if (noteErr) {
+            res.json(response(500, noteErr))
+        } else {
+            database.query(`select * from files where noteId='${req.query.id}'`, (fileErr, fileData)=>{
+                if (fileErr) {
+                    res.json(response(500, fileErr))
+                    return
+                }
+                result.id = noteData[0].id
+                result.userId = noteData[0].userId
+                result.title = noteData[0].title
+                result.content = noteData[0].content
+                result.files = fileData
+                res.json(response(200, result))
+            })
+        }
+    })
 })
 
 module.exports = router

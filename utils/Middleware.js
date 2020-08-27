c5b9226aaf76801dace56db6a01fbe08382152a3
KBeginner <wyyking@126.com>
2020-08-27 10:42:21 +0800

    引入moment日期格式化中间件

--- utils/Middleware.js
+++ utils/Middleware.js
@@ -4,6 +4,7 @@ const session = require('express-session')      // 登录校验
 const multer = require('multer')                // 上传文件
 const fs = require('fs')
 const path = require('path')
+const moment = require('moment')
 // 公用文件
 const database = require('./connectDB')
 const response = require('./httpRes')
@@ -13,6 +14,7 @@ module.exports = {
     multer : multer,
     fs : fs,
     path : path,
+    moment : moment,
     router : express.Router(),
     database : database,
     response : response
